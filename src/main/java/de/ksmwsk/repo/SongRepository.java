26634db7e18d8c2577c28747b18d9f85e3c4d0a7
Christian Kosmowski <christian.kosmowski@de.rhenus.com>
2023-05-11 14:13:28 +0200

    Test for jpaspec

--- src/main/java/de/ksmwsk/repo/SongRepository.java
+++ src/main/java/de/ksmwsk/repo/SongRepository.java
@@ -4,12 +4,13 @@ import de.ksmwsk.model.SongEntity;
 import io.micronaut.data.mongodb.annotation.MongoRepository;
 import io.micronaut.data.mongodb.annotation.MongoUpdateOptions;
 import io.micronaut.data.repository.CrudRepository;
+import io.micronaut.data.repository.jpa.JpaSpecificationExecutor;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 @MongoRepository
-public abstract class SongRepository implements CrudRepository<SongEntity, String> {
+public abstract class SongRepository implements CrudRepository<SongEntity, String>, JpaSpecificationExecutor<SongEntity> {
     @Override
     @MongoUpdateOptions(upsert = true)
     public abstract <S extends SongEntity> S update(@Valid @NotNull S entity);
---
+++ src/main/resources/mongo-init.sh
@@ -0,0 +1,1 @@
+mongo songdb --eval 'db.createCollection("songs")'
